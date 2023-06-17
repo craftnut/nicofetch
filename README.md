00cf83e7783e7290608c5645e3616ffa92608942
Nico <contact@craftnut.wtf>
2023-06-16 22:57:34 -0400

    Add first arguments.
    Added the ability to skip platform details and skip non-specific details.

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ it should work on Windows, macOS, Linux, and FreeBSD.
 
 # Usage
 
-``nicofetch`` to fetch all available information.
+``nicofetch`` to fetch all available information,
+you can add the argument ``--skip-platform`` if you don't want platform-specific info,
+or ``--platform-only`` to only get platform-specific info.
--- nicofetch.py
+++ nicofetch.py
@@ -4,10 +4,16 @@
 import os
 import sys
 import psutil
+from argparse import ArgumentParser
 
 import platform_fetches
 
 import math
+
+parser = ArgumentParser()
+parser.add_argument("--skip-platform", action="store_true")
+parser.add_argument("--platform-only", action="store_true")
+args = parser.parse_args()
 
 user = os.getlogin()
 
@@ -17,20 +23,22 @@ class colors:
     end = "\033[0m"
     
 # if the user is on linux print related system details first
-if sys.platform == "linux":
-    platform_fetches.fetch_linux()
-elif sys.platform == "win32":
-    platform_fetches.fetch_windows()
-elif sys.platform == "darwin":
-    platform_fetches.fetch_darwin()
-else:
-    print("Unsupported platform.")
+if not args.skip_platform:
+    if sys.platform == "linux":
+        platform_fetches.fetch_linux()
+    elif sys.platform == "win32":
+        platform_fetches.fetch_windows()
+    elif sys.platform == "darwin":
+        platform_fetches.fetch_darwin()
+    else:
+        print("Unsupported platform.")
 
 # universal  details, should work for every platform
-res = f"{os.get_terminal_size().columns}x{os.get_terminal_size().lines}"
-mem = math.ceil((psutil.virtual_memory()[0])/(1024**3))
-mem_usage = psutil.virtual_memory()[2]
+if not args.platform_only:
+    res = f"{os.get_terminal_size().columns}x{os.get_terminal_size().lines}"
+    mem = math.ceil((psutil.virtual_memory()[0])/(1024**3))
+    mem_usage = psutil.virtual_memory()[2]
 
-print(f"{colors.blue}Terminal Size:{colors.end}", res)
-print(f"{colors.blue}RAM:{colors.end}", str(mem) + "gb")
-print(f"{colors.blue}RAM Usage:{colors.end}", str(mem_usage) + "%")
+    print(f"{colors.blue}Terminal Size:{colors.end}", res)
+    print(f"{colors.blue}RAM:{colors.end}", str(mem) + "gb")
+    print(f"{colors.blue}RAM Usage:{colors.end}", str(mem_usage) + "%")
